95a445bcb702c4a008ffdca7ac0207c31f72790b
Pallav Dubey <46514947+pallav12@users.noreply.github.com>
2021-11-02 19:33:42 +0530

    Update NeuralNetwork.java

--- src/neuralNetwork/library/NeuralNetwork.java
+++ src/neuralNetwork/library/NeuralNetwork.java
@@ -1,40 +1,148 @@
-package neuralNetwork.library;
+package FFNN;
 
-import java.util.Random;
-
-import pallav.Matrix.Matrix;
+import java.util.ArrayList;
+import static FFNN.Variables.*;
 
 public class NeuralNetwork {
-	int input;
-	int hidden;
-	int output;
-	Matrix hi;
-	private NeuralNetwork(int input,int hidden, int output) {
-		this.input=input;
-		this.hidden=hidden;
-		this.output=output;
-		hi=Matrix.array(new float[hidden][input]);
-		randomize(hi);
-	}
-	public static NeuralNetwork init(int input, int hidden, int output) {
-		return new NeuralNetwork(input,hidden, output);
-	}
-	
-	public void feedForward(float[] arr) {
-		float [][] a=new float[arr.length][0];
-		for(int i=0;i<arr.length;i++) {
-			a[i][0]=arr[i];
-		}
-		Matrix mat=Matrix.array(a);
-		
-	}
-	private void randomize(Matrix m) {
-		for(int i=0;i<m.dimensions(m)[0];i++) {
-			for(int j=0;j<m.dimensions(m)[1];j++) {
-				Random rd=new Random();
-				m.array[i][j]=rd.nextFloat();
-			}
-		}		
-	}
-	
+
+    public NeuralNetwork(ArrayList<Integer> topology)
+    {
+        m_error = 0;
+        m_recentAverageError = 0;
+        m_recentAverageSmoothingFactor = definedRecentAverageSmoothingFactor;
+        int numLayers = topology.size();
+        System.out.println("Number of layers: " + numLayers);
+        m_layers = new ArrayList<>();
+        m_layers.clear();
+        for (int layerNum = 0; layerNum < numLayers; layerNum++)
+        {
+            m_layers.add(new Layer());
+            int numOutputs = layerNum == topology.size() - 1 ? 0 : topology.get(layerNum + 1);
+            for (int neuronNum = 0; neuronNum <= topology.get(layerNum); neuronNum++)
+            {
+                m_layers.get(m_layers.size()-1).add(new Neuron(numOutputs, neuronNum));
+                System.out.println("Made a neuron: " + neuronNum);
+            }
+
+            m_layers.get(m_layers.size()-1).peekLast().setOutputValue(1.0f);
+        }
+    }
+    public void feedForward(ArrayList<Float> inputValues)
+    {
+        assert(inputValues.size() == m_layers.get(0).size() - 1);
+
+        // Assign (latch) the input values into the input neurons
+        for (int i = 0; i < inputValues.size(); i++)
+        {
+            m_layers.get(0).get(i).setOutputValue(inputValues.get(i));
+        }
+
+        for (int layerNum = 1; layerNum < m_layers.size(); layerNum++)
+        {
+            Layer prevLayer = m_layers.get(layerNum - 1);
+            for (int n = 0; n < m_layers.get(layerNum).size() - 1; n++)
+            {
+                m_layers.get(layerNum).get(n).feedForward(prevLayer);
+            }
+        }
+    }
+    public void backProp(ArrayList<Float> targetValues)
+    {
+        Layer outputLayer = m_layers.get(m_layers.size()-1);
+        m_error = 0.0f;
+
+        for (int n = 0; n < outputLayer.size() - 1; n++)
+        {
+            float delta = targetValues.get(n) - outputLayer.get(n).getOutputValue();
+            m_error += delta * delta;
+        }
+        m_error /= outputLayer.size() - 1; //get average errorsquared
+        m_error = (float)Math.sqrt(m_error); // RMS
+
+        // Implement a recent average measurement;
+
+        m_recentAverageError =
+                (m_recentAverageError * m_recentAverageSmoothingFactor + m_error)
+                        / (m_recentAverageSmoothingFactor + 1.0f);
+
+        // Calculate output layer gradients
+        for (int n = 0; n < outputLayer.size() - 1; n++)
+        {
+            outputLayer.get(n).calcOutputGradients(targetValues.get(n));
+        }
+
+        // Calculate gradients on hidden layers
+        for (int layerNum = m_layers.size() - 2; layerNum > 0; layerNum--)
+        {
+            Layer hiddenLayer = m_layers.get(layerNum);
+            Layer nextLayer = m_layers.get(layerNum + 1);
+
+            for (int n = 0; n < hiddenLayer.size(); n++)
+            {
+                hiddenLayer.get(n).calcHiddenGradients(nextLayer);
+            }
+        }
+
+        // For all layers from outputs to first hidden layer.
+        // update connection weights
+
+        for (int layerNum = m_layers.size() - 1; layerNum > 0; layerNum--)
+        {
+            Layer layer = m_layers.get(layerNum);
+            Layer prevLayer = m_layers.get(layerNum - 1);
+
+            for (int n = 0; n < layer.size() - 1; n++)
+            {
+                layer.get(n).updateInputWeights(prevLayer);
+            }
+        }
+    }
+    public void getResults(ArrayList<Float> resultValues)
+    {
+        resultValues.clear();
+
+        for (int n = 0; n < m_layers.get(m_layers.size()-1).size() - 1; n++)
+        {
+            resultValues.add(m_layers.get(m_layers.size()-1).get(n).getOutputValue());
+        }
+    }
+    public float getNeuronOutput(int x, int y)
+    {
+        return m_layers.get(x).get(y).getOutputValue();
+    }
+    public float getRecentAverageError() { return m_recentAverageError; }
+
+    public void saveNeuronWeights()
+    {
+        neuronIndex = 0;
+        // Forward propagate
+        for (int layerNum = 1; layerNum < m_layers.size(); layerNum++)
+        {
+            Layer prevLayer = m_layers.get(layerNum - 1);
+            for (int n = 0; n < m_layers.get(layerNum).size() - 1; n++)
+            {
+                m_layers.get(layerNum).get(n).saveInputWeights(prevLayer);
+            }
+        }
+    }
+
+    public void loadNeuronWeights()
+    {
+        neuronIndex = 0;
+        // Forward propagate
+        for (int layerNum = 1; layerNum < m_layers.size(); layerNum++)
+        {
+            Layer prevLayer = m_layers.get(layerNum - 1);
+            for (int n = 0; n < m_layers.get(layerNum).size() - 1; n++)
+            {
+                m_layers.get(layerNum).get(n).loadInputWeights(prevLayer);
+            }
+        }
+    }
+
+    private ArrayList<Layer> m_layers; // m_layers[layerNum][neuronNum]
+    private float m_error;
+    private float m_recentAverageError;
+    private float m_recentAverageSmoothingFactor;
+
 }
