b95a09e029f983d0595483ea9e8451872be71011
Pallav Dubey <46514947+pallav12@users.noreply.github.com>
2021-11-02 19:31:15 +0530

    Create Edge.java

---
+++ src/neuralNetwork/library/Edge.java
@@ -0,0 +1,12 @@
+package FFNN;
+
+public class Connection {
+   public float weight;
+   public float deltaWeight;
+
+   public Connection()
+   {
+      weight = 0.0f;
+      deltaWeight = 0.0f;
+   }
+}
