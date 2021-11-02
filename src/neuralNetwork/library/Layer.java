c42d545ddc66e6ec49978a7c955ea2a457680936
Pallav Dubey <46514947+pallav12@users.noreply.github.com>
2021-11-02 19:30:17 +0530

    Create Layer.java

---
+++ src/neuralNetwork/library/Layer.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class Layer extends ArrayList<Neuron> {
+
+    public Layer()
+    {
+        super();
+    }
+
+    @Override
+    public boolean add(Neuron aNeuron) {
+        return super.add(aNeuron);
+    }
+
+    public Neuron pollLast() {
+        Neuron neuron = peekLast();
+        this.remove(super.get(this.size()-1));
+        return neuron;
+    }
+
+    public Neuron peekFirst() {
+        return super.get(0);
+    }
+
+    public Neuron peekLast() {
+        return super.get(this.size()-1);
+    }
+
+    @Override
+    public void clear() {
+        super.clear();
+    }
+
+    @Override
+    public int size() {
+        return super.size();
+    }
+}
